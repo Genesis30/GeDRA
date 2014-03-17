6c2a8e2b957dfb28cd3cbfed8cd59d16d0366422
Genesis30 <carlos.bevia@gmail.com>
2014-03-17 12:03:59 +0100

    Basic implementation & xml model

--- xmlReading.py
+++ xmlReading.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as et
 
-root = ET.fromstring()
+tree = et.parse('model.xml')
+root = tree.getroot()
 
+for part in root:
+	print part.tag
+	print '-------'
+	for element in part:
+		print element.tag, element.attrib
+		for charac in element:
+			print charac.tag
+		print '**** \n'
+
+print et.tostring(root, encoding="us-ascii", method="xml")
